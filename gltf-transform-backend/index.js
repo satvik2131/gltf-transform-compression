3843d07d8160bbdfc12e0cf83029b938b4d1acbc
satvik213161 <satvik213161@gmail.com>
2023-06-05 18:10:18 +0530

    basic done (buffer to gltf conversion issue)

--- gltf-transform-backend/index.js
+++ gltf-transform-backend/index.js
@@ -1,82 +1,93 @@
-const express = require('express')
-const app = express()
-const port = 3001
-const { NodeIO, Document } = require('@gltf-transform/core');
-const { KHRONOS_EXTENSIONS } = require('@gltf-transform/extensions');
-const { resample, prune, dedup, draco, center, simplify } = require('@gltf-transform/functions');
-const draco3d = require('draco3d');
-const fs = require('fs');
-const { MeshoptSimplifier } = require('meshoptimizer');
-const { log } = require('console');
-const cors = require('cors');
-const formidable = require('formidable');
-const { join } = require('node:path');
-const { readFile } = require('node:fs/promises');
-
+const express = require("express");
+const app = express();
+const port = 3001;
+const { NodeIO, Document } = require("@gltf-transform/core");
+const { KHRONOS_EXTENSIONS } = require("@gltf-transform/extensions");
+const {
+  resample,
+  prune,
+  dedup,
+  draco,
+  center,
+  simplify,
+} = require("@gltf-transform/functions");
+const draco3d = require("draco3d");
+const fs = require("fs");
+const { MeshoptSimplifier } = require("meshoptimizer");
+const { log } = require("console");
+const cors = require("cors");
+const formidable = require("formidable");
 
 app.use(cors());
 
-app.post('/transform/model', async (req, res) => {
-    const form = formidable({ multiples: true });
+app.post("/transform/model", async (req, res) => {
+  const form = formidable({ multiples: true });
 
-    form.parse(req, async (err, fields, files) => {
-        if (err) {
-            res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
-            res.end(String(err));
-            return;
-        }
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      res.writeHead(err.httpCode || 400, { "Content-Type": "text/plain" });
+      res.end(String(err));
+      return;
+    }
 
-        //Input filepath
-        // const path = files.file.filepath;
-        //postman input
-        const path = files[''].filepath;
+    //Input filepath
+    const path = files.file.filepath;
+    //Postman input
+    // const path = files[""].filepath;
 
-        // Configure I / O.
-        const io = new NodeIO()
-            .registerExtensions(KHRONOS_EXTENSIONS)
-            .registerDependencies({
-                'draco3d.decoder': await draco3d.createDecoderModule(), // Optional.
-                'draco3d.encoder': await draco3d.createEncoderModule(), // Optional.
-            });
+    // Configure I / O.
+    const io = new NodeIO()
+      .registerExtensions(KHRONOS_EXTENSIONS)
+      .registerDependencies({
+        "draco3d.decoder": await draco3d.createDecoderModule(), // Optional.
+        "draco3d.encoder": await draco3d.createEncoderModule(), // Optional.
+      });
 
-        // Read.
-        let document = await io.read(path); // → Document
-        await document.transform(
-            // Losslessly resample animation frames.
-            resample(),
-            // Remove unused nodes, textures, or other data.
-            prune(),
-            // Remove duplicate vertex or texture data, if any.
-            dedup(),
-            // Compress mesh geometry with Draco.
-            draco(),
-            //centers the model at the origin
-            center(),
-            //prduces fewer triangles and meshes
-            simplify({ simplifier: MeshoptSimplifier, ratio: 0.2, error: 0.0001 })
-        );
+    // Read.
+    let document = await io.read(path); // → Document
+    await document.transform(
+      // Losslessly resample animation frames.
+      resample(),
+      // Remove unused nodes, textures, or other data.
+      prune(),
+      // Remove duplicate vertex or texture data, if any.
+      dedup(),
+      // Compress mesh geometry with Draco.
+      draco(),
+      //centers the model at the origin
+      center(),
+      //prduces fewer triangles and meshes
+      simplify({ simplifier: MeshoptSimplifier, ratio: 0.2, error: 0.0001 })
+    );
 
-        const byteArrayGlb = await io.writeBinary(document);
-
-
-        //To save file in system
-        fs.writeFile('test2.glb', byteArrayGlb, (err) => {
-            if (err) throw err;
-            console.log('The file has been saved!');
-        });
-
-        // const filePath = new URL('test2.glb');
-        // const contents = await readFile(filePath);
-        // log(contents);
-
-        // res.write(byteArrayGlb, 'binary');
-        // res.end();
-
-        // res.writeHead(200, { 'Content-Type': 'application/json' });
-        // res.end(JSON.stringify({ fields, files }, null, 2));
+    //Sending buffer
+    const data = await io.writeBinary(document);
+    const mimetype = "application/octet-stream";
+    res.writeHead(200, {
+      "Content-Type": mimetype,
+      "Content-disposition": "attachment;filename=" + "model.glb",
+      "Content-Length": data.length,
     });
-})
+    res.end(Buffer.from(data, "binary"));
+
+    //Sending JSON
+    // const data = await io.writeJSON(document);
+    // const mimetype = "application/octet-stream";
+    // res.writeHead(200, {
+    //   "Content-Type": mimetype,
+    //   "Content-disposition": "attachment;filename=" + "model.glb",
+    //   "Content-Length": data.length,
+    // });
+    // res.end(data);
+
+    //To save file in system
+    // fs.writeFile('test2.glb', byteArrayGlb, (err) => {
+    //     if (err) throw err;
+    //     console.log('The file has been saved!');
+    // });
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- gltf-transform-frontend/src/Compression.js
+++ gltf-transform-frontend/src/Compression.js
@@ -3,32 +3,27 @@ import { useLoader, useThree } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "@react-three/drei";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
-import { ObjectLoader } from "three";
+import { Buffer } from "buffer";
 
-export function Model({ json }) {
+export function Model({ data }) {
+  const buffer = Buffer.from(data);
+  const gltfArrBuffer = buffer.buffer;
+  const loader = new GLTFLoader();
+  loader.parse(gltfArrBuffer, (dt) => {
+    console.log(dt);
+  });
 
-    // const json = model.parser.json;
-    // const objModel = useLoader(loaders.obj, json);
-    // const dracoLoader = new DRACOLoader();
-    // dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/');
-    // dracoLoader.setDecoderConfig({ type: 'js' });
-    // const gltf = useLoader(GLTFLoader, url, (loader) => {
-    //     loader.setDRACOLoader(dracoLoader);
-    // });
-    const { scene } = useThree();
-    const gltf = new ObjectLoader(json);
-    scene.add(gltf);
+  // const gltf = new ObjectLoader(json);
+  // scene.add(gltf);
 
-
-    return (
-        <group>
-            <OrbitControls />
-            <ambientLight intensity={0.5} />
-            <directionalLight intensity={1} position={[0, 10, 0]} />
-            <primitive object={scene} />
-        </group>
-    );
+  return (
+    <group>
+      <OrbitControls />
+      <ambientLight intensity={0.5} />
+      <directionalLight intensity={1} position={[0, 10, 0]} />
+      {/* <primitive object={scene} /> */}
+    </group>
+  );
 }
-
 
 export default Model;
