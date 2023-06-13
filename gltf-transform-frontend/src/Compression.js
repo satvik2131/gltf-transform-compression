1946a121b61df21abbad71bdcf050595a761d0f3
satvik213161 <satvik213161@gmail.com>
2023-06-13 17:52:22 +0530

    centering done !

--- gltf-transform-frontend/src/Compression.js
+++ gltf-transform-frontend/src/Compression.js
@@ -3,6 +3,7 @@ import { Canvas, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "@react-three/drei";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { Center, Bounds, Resize } from "@react-three/drei";
 
 export function Model({ model }) {
   console.log(model);
@@ -77,12 +78,16 @@ export function Model({ model }) {
   // scene.add(gltf);
 
   return (
-    <mesh position={[0, 0, 0]} scale={1.0}>
-      <OrbitControls />
-      <ambientLight intensity={0.5} />
-      <directionalLight intensity={1} position={[0, 10, 0]} />
-      <primitive object={gltf.scene} />
-    </mesh>
+    <Bounds fit clip observe>
+      <Resize scale={1}>
+        <Center>
+          <OrbitControls />
+          <ambientLight intensity={0.5} />
+          <directionalLight intensity={1} position={[0, 10, 0]} />
+          <primitive object={gltf.scene} />
+        </Center>
+      </Resize>
+    </Bounds>
   );
 }
 
